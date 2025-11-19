8b689a4d1c3cb0970ef7d74139afdc18b1b4a5f5
Lucas Priez <lucas.priez@etud.u-picardie.fr>
2025-11-19 17:32:15 +0100

    fix(TestsParcours): Fix d'un namespace d'un import

--- UniversiteDomainUnitTest/ParcoursUnitTest.cs
+++ UniversiteDomainUnitTest/ParcoursUnitTest.cs
@@ -2,8 +2,8 @@ using Moq;
 using UniversiteDomain.DataAdapters;
 using UniversiteDomain.DataAdapters.DataAdaptersFactory;
 using UniversiteDomain.Entities;
+using UniversiteDomain.UseCases.ParcoursUseCases.Create;
 using UniversiteDomain.UseCases.ParcoursUseCases.EtudiantDansParcours;
-using UniversiteDomain.UseCases.ParcourUseCases.Create;
 
 namespace UniversiteDomainUnitTests;
 
